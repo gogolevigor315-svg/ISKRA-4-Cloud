1b3afb42a91c90936961822e9c2ca9cf2f8f7e6e
gogolevigor315-svg <gogolevigor315@gmail.com>
2026-02-04 21:25:23 +0200

    Update __init__.py

--- iskra_modules/ symbiosis_core/__init__.py
+++ iskra_modules/ symbiosis_core/__init__.py
@@ -1,1 +1,21 @@
+"""
+ISKRA-4 · SYMBIOSIS-CORE v5.4
+Надсистема симбиоза для эволюции ISKRA-4
+"""
 
+from .symbiosis_core import SymbiosisCore
+from .aladdin_shadow import AladdinShadow, AladdinShadowSync
+from .symbiosis_api import symbiosis_bp
+from .iskra_integration import ISKRAAdapter
+
+__version__ = "5.4"
+__author__ = "ISKRA-4"
+__description__ = "Сефиротический симбионт с теневой аналитикой"
+
+__all__ = [
+    'SymbiosisCore',
+    'AladdinShadow',
+    'AladdinShadowSync',
+    'symbiosis_bp',
+    'ISKRAAdapter'
+]
