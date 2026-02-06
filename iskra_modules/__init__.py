c85b13eef5c722e646c705160382e24209c68ea7
gogolevigor315-svg <gogolevigor315@gmail.com>
2026-02-06 20:12:55 +0200

    Update __init__.py

--- iskra_modules/__init__.py
+++ iskra_modules/__init__.py
@@ -1,1 +1,9 @@
+# iskra_modules/__init__.py
+"""ISKRA Modules Package"""
+__version__ = '1.0.0'
 
+# Экспорт основных модулей
+from . import symbiosis_core
+from . import sephirot_blocks
+
+__all__ = ['symbiosis_core', 'sephirot_blocks']
